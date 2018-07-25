0505b1fd15c9650f15774e2f3feaeff2a42b75b1
Sara Kim <sarakim0903@gmail.com>
2018-07-25 21:13:54 +0000

    automatically backed up by learn

---
+++ loop.rb
@@ -0,0 +1,30 @@
+# def example
+#   counter = 1 
+#   while counter <= 9 
+#   puts "I love cs"
+#   counter += 1 
+#   end
+# end 
+
+
+
+
+# def ex2
+#   counter = 0 
+#   until counter == 9 
+#   puts "I love cs"
+#   counter += 1 
+#   end
+# end
+
+loop do
+  puts "dfs"
+  puts"2"
+  puts "3"
+  puts "4"
+  break
+end
+
+puts "do leg"
+puts "do leg"
+exit
